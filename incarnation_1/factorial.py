4d12500e4928aa1d2f475e0c5ce247efb42484b7
Goutham Susarla <gouthamsusarla@gmail.com>
2023-03-15 19:39:25 -0400

    Update factorial.py
    Added conditions to validate the integers and comments.

--- incarnation_1/factorial.py
+++ incarnation_1/factorial.py
@@ -1,5 +1,16 @@
 def factorial(n):
+    # Check for negative integer
+    if n < 0:
+        return None
+    # Initialize result variable
     result = 1
-    for i in range(2, n+1):
-        result *= i
+    # Iterate from 1 to n
+    for i in range(1, n+1):
+        try:
+            # Multiply each number to the result variable
+            result *= i
+        except:
+            # If there is an error, return None
+            return None
+    # Return the result variable
     return result
